4dc824c7e7413f7927e2d761bf1375a2f2bee80e
vinodh1988 <k.c.vinodh@hotmail.com>
2025-03-03 16:45:13 +0530

    alert

--- README.md
+++ README.md
@@ -34,3 +34,43 @@ There are three types of angular directives
     if show is false
 
     Note: structural directives are prefixed with *
+### Attribute directives
+
+Attribute directives are further classifed into three kinds 
+* Input directive
+* Output directive
+* Input Output directive
+
+* Input Directive
+   
+   Input directives are directives that are applied
+on target elements (html elements or component) if
+applied it would change the behavior of the target element
+data change, style size
+
+    some inbuilt attribute directives : style,ngStyle,ngClass
+
+ Input directives receives values from the right
+ The values must be variables
+ Input directives are enclosed within []
+
+ <h1 [style.color]='mycolor'>Hello</h1>
+
+   here mycolor is a variable , if mycolor=green the h1 will be green
+
+ <h1 [ngClass]='classname'>Data </h1>
+   
+   here classname is a variable classname='alert alert-info' that value
+   will be applied
+
+   in case if you want to substitute direct values
+
+<h1 ngClass="alert alert-info"></h1>
+
+<h1 [color]="mycolor"></h1>
+
+mycolor is a variable
+
+<h1 color="red"> </h1>
+
+red is a value
--- src/app/info-item/info-item.component.ts
+++ src/app/info-item/info-item.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-info-item',
   templateUrl: './info-item.component.html',
   styleUrl: './info-item.component.css'
 })
-export class InfoItemComponent {
-  message:string = 'This is an info item';
-}
+
+  export class InfoItemComponent implements OnInit {
+    message: string = 'This is an info item';
+    alertTypes: string[] = ['alert alert-primary', 'alert alert-secondary', 'alert alert-success', 
+      'alert alert-danger', 'alert alert-warning', 'alert alert-info', 
+      'alert alert-light', 'alert alert-dark'];
+    bootclass: string = this.alertTypes[0];
+    constructor(@Inject(PLATFORM_ID) private platformId: Object) { }
+    ngOnInit() {
+     if(isPlatformBrowser(this.platformId)) {
+        setInterval(() => {
+          const currentIndex = this.alertTypes.indexOf(this.bootclass);
+          const nextIndex = (currentIndex + 1) % this.alertTypes.length;
+          this.bootclass = this.alertTypes[nextIndex];
+        }
+     ,10000)
+    }
+  }
+  }
